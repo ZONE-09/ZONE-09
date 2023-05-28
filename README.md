3241c3badb7f1a4960f4006ffe933b63cff375cd
Kardama Devahuti <kardama.devahuti@protonmail.com>
2023-05-28 07:20:57 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+  <img  src="https://www.eltonyoga.com/wp-content/uploads/2020/08/SamkhyaC.png">
+</p>
---
+++ script.js
@@ -0,0 +1,7 @@
+$("#target1").click(function() {
+  window.location.href = "https://en.wikipedia.org/wiki/Rabindranath_Tagore"
+});
+
+$("#target2").click(function() {
+  window.open("http://www.sacred-texts.com/hin/tagore/gitnjali.htm");
+});
